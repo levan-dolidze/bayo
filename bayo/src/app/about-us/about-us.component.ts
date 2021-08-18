c62830d1b872fea3bb139c652a0a311fd8ad96b4
DESKTOP-H8VL884\levan <levan.dolidze@outlook.com>
2021-08-18 22:54:38 +0400

    about us and brewery pages development

--- bayo/src/app/about-us/about-us.component.ts
+++ bayo/src/app/about-us/about-us.component.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormControlDirective, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
+
 export class AboutUsComponent implements OnInit {
-  
+
   constructor() { }
 
 
@@ -15,5 +17,6 @@ export class AboutUsComponent implements OnInit {
 
 
   }
+
 
 }
