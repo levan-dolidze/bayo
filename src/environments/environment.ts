3ff873aeee2fc62f10d34c56c82c824e8335412a
DESKTOP-H8VL884\levan <levan.dolidze@outlook.com>
2021-08-30 11:38:56 +0400

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase:{
-    apiKey: "AIzaSyA9qmGAIPsp3VHPtVZfgVoVkxuGg3VP5gk",
-    authDomain: "bayocontact-a7f9d.firebaseapp.com",
-    databaseURL: "https://bayocontact-a7f9d-default-rtdb.firebaseio.com",
-    projectId: "bayocontact-a7f9d",
-    storageBucket: "bayocontact-a7f9d.appspot.com",
-    messagingSenderId: "1060647286288",
-    appId: "1:1060647286288:web:c82c03fc912fcd00c87c95",
-    measurementId: "G-T54ZYY0M0G"
-  }
+  production: false
+
 };
 
 /*
